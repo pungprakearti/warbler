6f63a336becdfc2bc98167214a5da21ff7122105
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-10-12 15:18:22 -0700

    Made some changes to app.py for heroku.

--- app.py
+++ app.py
@@ -16,10 +16,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgres:///warbler'))
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+#     'DATABASE_URL', 'postgres:///flask-heroku')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "illnevertell")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
